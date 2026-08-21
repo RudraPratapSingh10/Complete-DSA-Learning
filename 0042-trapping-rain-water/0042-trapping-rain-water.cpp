a68769ade92fa4d3677f02c11beef439950fb43b
Rudra Pratap SIngh <190636408+RudraPratapSingh10@users.noreply.github.com>
2026-08-22 02:07:30 +0530

    Time: 2 ms (18.57%), Space: 27.4 MB (6.19%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,58 +1,32 @@
 class Solution {
 public:
-    int trap(vector<int>& height) {
-
-        int n = height.size();
-        int count = 0;
-
-        int s = 0;
-
-        while (s < n - 1) {
-
-            // zero height ko skip karo
-            if (height[s] == 0) {
-                s++;
-                continue;
-            }
-
-            int e = s + 1;
-            int best = -1;
-
-            // right side me boundary search karo
-            while (e < n && height[e] < height[s]) {
-
-                // sabse badi chhoti boundary store karo
-                if (best == -1 || height[e] > height[best]) {
-                    best = e;
-                }
-
-                e++;
-            }
-
-            // Case 1: height[s] se badi/equal boundary mil gayi
-            if (e < n) {
-
-                for (int i = s + 1; i < e; i++) {
-                    count += height[s] - height[i];
-                }
-
-                s = e;
-            }
-
-            // Case 2: badi boundary nahi mili
-            else {
-
-                if (best == -1)
-                    break;
-
-                for (int i = s + 1; i < best; i++) {
-                    count += height[best] - height[i];
-                }
-
-                s = best;
-            }
+    vector<int> getLeftMax(vector<int>& height, int n) {
+        vector<int> leftMax(n);
+        leftMax[0] = height[0];
+        for(int i = 1; i<n; i++) {
+            leftMax[i] = max(leftMax[i-1], height[i]);
         }
-
-        return count;
+        return leftMax;
+    }
+    vector<int> getRightMax(vector<int>& height, int n) {
+        vector<int> rightMax(n);
+        rightMax[n-1] = height[n-1];
+        for(int i = n-2; i>=0; i--) {
+            rightMax[i] = max(rightMax[i+1], height[i]);
+        }
+        return rightMax;
+    } 
+    int trap(vector<int>& height) {
+        int n = height.size();
+        if(n == 1 || n == 0)
+            return 0;
+        vector<int> leftMax = getLeftMax(height, n);
+        vector<int> rightMax = getRightMax(height, n);
+        int sum = 0;
+        for(int i = 0; i<n; i++) {
+            sum += min(leftMax[i], rightMax[i]) - height[i];
+        }
+        return sum;
     }
 };
+
