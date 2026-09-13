45db0bc1c229cd4f6c0ef1e60577825ffbe72986
Rudra Pratap SIngh <190636408+RudraPratapSingh10@users.noreply.github.com>
2026-09-14 00:24:08 +0530

    Time: 1005 ms (82.98%), Space: 0B (100%) - LeetHub

---
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -0,0 +1,4 @@
+select s.student_id,s.student_name,sb.subject_name ,count(e.subject_name) as attended_exams from students s 
+cross join  subjects sb left join examinations e    on s.student_id = e.student_id AND sb.subject_name = e.subject_name
+GROUP BY s.student_id, s.student_name, sb.subject_name
+ORDER BY s.student_id, sb.subject_name;
